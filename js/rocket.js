a599b6b6daf4b75fd3f4e3faf18ac931b514eee6
lilyelle <lilylboyce@gmail.com>
2016-03-17 13:08:25 -0400

    made my rocket!

--- js/rocket.js
+++ js/rocket.js
@@ -3,12 +3,36 @@ var countdownNumber = 10;
 
 var changeState = function (state) {
 	document.body.className = 'body-state' + state;
+	clearInterval(timer);
+	countdownNumber = 10;
 
+	// countdown
 	if (state == 2) {
 		timer = setInterval(function () {
-			document.getElementByID ('countdown').innerHTML = countdownNumber;
 			countdownNumber = countdownNumber - 1;
-		}, 1000);
-		
+			document.getElementById ('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber <= 0) {
+				changeState(3);
+			}
+		}, 500);
+
+
+	} else if (state ==3){
+		var success = setTimeout(function ()
+			{
+				var randomNumber = Math.round(Math.random()*10);
+
+				console.log('randomNumber:',randomNumber)
+
+				//success!
+				if (randomNumber > 3) {
+					changeState(4);
+
+				} else {
+					changeState(5); //failure!
+				}
+		}, 2000);
 	};
+	
 }
